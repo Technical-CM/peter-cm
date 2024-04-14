1a4dc1108c329513c1ffb2cc1c477f8817676eee
Technical-CM <106469151+Technical-CM@users.noreply.github.com>
2024-04-14 18:36:05 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+<!--head starts -->
+<head>
+    <?php include 'head.php'; ?>
+</head>
+<!--head ends -->
+
+    <body>
+        <!-- spinner starts -->
+        <!-- spinner end -->
+
+        <!-- main starts -->
+        <?php include 'body.php'; ?>
+        <!-- main ends -->
+
+
+        <!--  scripts starts -->
+        <?php include 'scripts.php'; ?>
+        <!--  scripts ends -->
+    </body>
+
+</html>
